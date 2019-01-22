f7512b7073dfbe3b986db74b8ad4b6b9258c11be
Alexander <alex.c.hayes08@gmail.com>
2019-01-22 01:45:03 -0600

    Latest.

---
+++ ApertureLabs.Selenium/Js/Block.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApertureLabs.Selenium.Js
+{
+    /// <summary>
+    /// Surrounds a bit of js with brackets.
+    /// </summary>
+    public class Block : JavaScript
+    {
+        #region Constructor
+
+        internal Block()
+        {
+            Before = String.Empty;
+            Content = new List<JavaScript>();
+            After = String.Empty;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// What's placed before the start of the brackets.
+        /// </summary>
+        public string Before { get; set; }
+
+        /// <summary>
+        /// Will be put after the brackets.
+        /// </summary>
+        public string After { get; set; }
+
+        /// <summary>
+        /// Placed inside the brackets.
+        /// </summary>
+        public IEnumerable<JavaScript> Content { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(Before);
+            sb.Append("{");
+
+            foreach (var s in Content)
+                sb.Append(s);
+
+            sb.Append("}");
+            sb.Append(After);
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Creates a function js block.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="arguments"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static Block Function(string name,
+            IEnumerable<string> arguments,
+            IEnumerable<JavaScript> content)
+        {
+            var block = new Block();
+
+            block.Before = string.Format("function {0} ({1})",
+                name,
+                String.Join(",", arguments));
+
+            block.Content = content;
+
+            return block;
+        }
+
+        #endregion
+    }
+}
---
+++ ApertureLabs.Selenium/Js/Statement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApertureLabs.Selenium.Js
+{
+    public class Statement : JavaScript
+    {
+        #region Constructor
+
+        public Statement()
+        {
+            Content = String.Empty;
+        }
+
+        public Statement(string content)
+        {
+            Content = content;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Content { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
